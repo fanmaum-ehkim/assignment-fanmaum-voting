b1d89a51e5ec1f70a0c0556500b9f7b6cce1e121
eh.kim <eh.kim@fanmaum.com>
2025-09-01 18:35:02 +0900

    add: user 생성 기능 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  async getUserById(id: number) {
-    return this.userService.findById(id);
+  @Post()
+  @ApiOperation({ summary: '유저 생성' })
+  async createUser(@Body() data: CreateUserDto) {
+    await this.userService.createUser(data);
+    return { message: 'User 생성 완료' };
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(private prismaService: PrismaService) {}
 
-  async findById(id: number) {
-    return this.prismaService.user.findUnique({
-      where: { id },
+  async createUser(data: CreateUserDto) {
+    return this.prismaService.user.create({
+      data,
     });
   }
 }
